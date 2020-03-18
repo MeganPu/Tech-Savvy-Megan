2c135f0299a69f4f0af886f1529ba10f00c72a3c
MeganPu <62252428+MeganPu@users.noreply.github.com>
2020-03-18 12:59:19 -0400

    Finish first intro class to python

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
 print('hello,world')
+print(2020-1999)
+
+# what is value?
+#what is operator?
+
+print(2+2*3)
+name = input('Please enter your name')
+print('Hello,',name) 
+grade = 'A'
+#left is variable, right is expression 
+print(name,'got a grade', grade)
