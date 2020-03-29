c59fd6035ec7f5a8dbd5892bf4205fd631df0083
MeganPu <62252428+MeganPu@users.noreply.github.com>
2020-03-28 21:26:54 -0400

    class3

---
+++ Class 3.py
@@ -0,0 +1,10 @@
+import turtle
+jack = turtle.Turtle()
+print(jack) #This tells us that jack refers to an object with type Turtle as defined in module turtle.
+turtle.mainloop()
+jack.fd(100)
+jack.lt(90)
+jack.fd(100)
+
+for i in range(4):
+    print('Hello!')
