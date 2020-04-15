f4bfa5d380f070a53a4ec5926119c0bdbdfbb0c6
MeganPu <62252428+MeganPu@users.noreply.github.com>
2020-04-14 23:02:39 -0400

    Update json_exercise.py

--- json_exercise.py
+++ json_exercise.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 import pprint
 
-APIKEY = 'YOUR_OWN_APIKEY'
+APIKEY = 'b05f16d2a80cb31bdf8dff023a23f661'
 city = 'Wellesley'
 country_code = 'us'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
@@ -12,3 +12,10 @@ f = urllib.request.urlopen(url)
 response_text = f.read().decode('utf-8')
 response_data = json.loads(response_text)
 pprint.pprint(response_data)
+
+print(type('main'))
+pprint.pprint(response_data['main'])
+print(response_data['main']['temp'])
+
+
+
